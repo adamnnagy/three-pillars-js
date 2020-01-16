481e2fe8d409fecfc3b6ef54211bed6008dddf11
Adam Nagy <an2208@nyu.edu>
2020-01-16 15:43:11 +0100

    solved

--- ex.js
+++ ex.js
@@ -1,43 +1,47 @@
 class Bookshelf {
-	constructor() {
-		this.favoriteBooks = [];
-	}
-
-	// TODO: define methods `addFavoriteBook(..)`
-	// and `printFavoriteBooks()`
+  constructor() {
+    this.favoriteBooks = [];
+  }
+  addFavoriteBook(bookName) {
+    if (!bookName.includes("Great")) {
+      this.favoriteBooks.push(bookName);
+    }
+  }
+  printFavoriteBooks() {
+    console.log(`Favorite Books: ${String(this.favoriteBooks.length)}`);
+    for (let bookName of this.favoriteBooks) {
+      console.log(String(bookName));
+    }
+  }
 }
 
-function addFavoriteBook(bookName) {
-	if (!bookName.includes("Great")) {
-		favoriteBooks.push(bookName);
+function loadBooks(bookshelfInstance) {
+  fakeAjax(BOOK_API, function(booklist) {
+    for (let i = 0; i < booklist.length; i++) {
+      bookshelfInstance.addFavoriteBook(booklist[i]);
 	}
-}
-
-function printFavoriteBooks() {
-	console.log(`Favorite Books: ${favoriteBooks.length}`);
-	for (let bookName of favoriteBooks) {
-		console.log(bookName);
-	}
-}
-
-function loadBooks( /* .. */ ) {
-	// TODO: call fakeAjax( .. );
+	bookshelfInstance.printFavoriteBooks();
+  });
 }
 
 var BOOK_API = "https://some.url/api";
+var myBookshelf = new Bookshelf();
+loadBooks(myBookshelf);
 
+// calling myBookshelf.printFavoriteBooks(); will not work on the global scope, because of context
 
 // ***********************
 
 // NOTE: don't modify this function at all
-function fakeAjax(url,cb) {
-	setTimeout(function fakeLoadingDelay(){
-		cb([
-			"A Song of Ice and Fire",
-			"The Great Gatsby",
-			"Crime & Punishment",
-			"Great Expectations",
-			"You Don't Know JS"
-		]);
-	},500);
+function fakeAjax(url, cb) {
+  setTimeout(function fakeLoadingDelay() {
+    cb([
+      "A Song of Ice and Fire",
+      "The Great Gatsby",
+      "Crime & Punishment",
+      "Great Expectations",
+      "You Don't Know JS"
+    ]);
+  }, 500);
 }
+
